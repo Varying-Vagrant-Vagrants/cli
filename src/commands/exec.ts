0a6662f4ac6916452df282cb09d7ffbe7fcc31cc
Tom J Nowell <contact@tomjn.com>
2025-12-28 20:22:43 +0000

    Add exec command for running commands in VVV VM
    - Execute arbitrary commands inside the VVV virtual machine
    - Pass commands directly via vagrant ssh
    - Support --json flag for automation

---
+++ src/commands/exec.ts
@@ -0,0 +1,21 @@
+import { Command } from "commander";
+import { DEFAULT_VVV_PATH } from "../utils/config.js";
+import { ensureVvvExists, ensureVvvRunning } from "../utils/cli.js";
+import { ensureVagrantInstalled, vagrantSshAndExit } from "../utils/vagrant.js";
+
+export const execCommand = new Command("exec")
+  .description("Execute a command inside the VVV virtual machine")
+  .option("-p, --path <path>", "Path to VVV installation", DEFAULT_VVV_PATH)
+  .argument("<command...>", "Command to execute inside the VM")
+  .action((commandParts, options) => {
+    const vvvPath = options.path;
+
+    ensureVvvExists(vvvPath);
+    ensureVagrantInstalled();
+    ensureVvvRunning(vvvPath);
+
+    // Join command parts back together
+    const command = commandParts.join(" ");
+
+    vagrantSshAndExit(command, vvvPath);
+  });
